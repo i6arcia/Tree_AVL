ef536c6d4f01405e1f8da748deafb746962d2617
Irvin <i6arcia@gmail.com>
2015-11-30 21:16:27 -0600

    Agregar Nodo

--- Luna_Tagano/Tree/src/tree/MyTree.java
+++ Luna_Tagano/Tree/src/tree/MyTree.java
@@ -12,10 +12,39 @@ public class MyTree {
     private Nodo insert(int dato, Nodo b){
         if (b == null)
             b = new Nodo(dato, null, null);
+        else{
+            if (dato == b.valor)
+                return b;
+            else if (dato < b.valor && b.facEquilibrio > -1){
+                b.izquierdo = insert(dato, b.izquierdo);
+            }else if (dato > b.valor){
+                b.derecho = insert (dato, b.derecho);
+            }else{
+                
+                return b;
+            }
+        }
+        b.profundidad = profMax(getProfundidad (b.izquierdo), getProfundidad (b.derecho)) + 1;
+        b.facEquilibrio = getFactorEquilibrio(b.izquierdo, b.derecho);
+        System.out.println("Val[" + b.valor + "] Fe["+ b.facEquilibrio+"] Pf["+b.profundidad+"]");
         return b;
     }
     
-    public void impPos_orden(){
+    private int profMax(int profIzq, int profDer){
+        return profIzq > profDer ? profIzq : profDer;
+    }
+    private int getProfundidad(Nodo b){
+        return b == null ? -1 : b.profundidad;
+    }
+    private int getFactorEquilibrio(Nodo izq, Nodo der){
+        int fEi, fEd;
+        fEi = izq == null ? -1 : izq.profundidad;
+        fEd = der == null ? -1 : der.profundidad;
+        return fEd- fEi;
+    }
+    
+    
+    public void impPost_orden(){
         
     }
     
--- Luna_Tagano/Tree/src/tree/Tree.java
+++ Luna_Tagano/Tree/src/tree/Tree.java
@@ -4,6 +4,11 @@ public class Tree {
 
     public static void main(String[] args) {
         
+        MyTree miArbol = new MyTree();
+        
+        miArbol.insertar(5);
+        miArbol.insertar(3);
+        
         Principal win = new Principal();
         win.setTitle("Trabajando con Arboles");
         win.setVisible(true);
